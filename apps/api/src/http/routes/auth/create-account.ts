9dbd477411607001464bab7e48fdfaef5b7cf3b9
ericopieri <erico.pieri@gmail.com>
2024-05-26 01:11:18 -0300

    rota para criação de usuarios implementada

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '../../../lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,34 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { email, password, name } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: 'There is already a user with this email.',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          email,
+          passwordHash,
+          name,
+        },
+      })
+
+      return reply.status(201).send({
+        message: 'Account created successfully.',
+      })
     },
   )
 }
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -7,12 +7,16 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 
+import { createAccount } from './routes/auth/create-account'
+
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
+
+app.register(createAccount)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server is running! 🚀')
